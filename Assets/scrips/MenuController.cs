edc49b7aa0e2009ac0329c45b368449a3a5df49e
Fernando Mendoza <fernandogx8647@gmail.com>
2025-01-26 14:36:40 -0600

    Menu

--- Assets/scrips/MenuController.cs
+++ Assets/scrips/MenuController.cs
@@ -3,18 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Método para cargar la escena del juego
     public void PlayGame()
     {
-        // Reemplaza "GameScene" con el nombre de la escena que quieres cargar
-        SceneManager.LoadScene("nivlel");
+        SceneManager.LoadScene("Dr_Mashup");
         Time.timeScale = 1f;
     }
 
-    // Método para salir del juego
     public void QuitGame()
     {
         Application.Quit();
-        Debug.Log("Saliendo del juego..."); // Solo visible en el editor
+        Debug.Log("Saliendo del juego..."); 
     }
 }
